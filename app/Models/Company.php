fa8f6d43b823374d110f01ce879cecf716de6a8c
Ryobi <robiatmaja7@gmail.com>
2025-05-01 10:45:17 +0700

    feat: Add user profile management

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'logo',
+    ];
+
+    // realtion company with users
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    // relation company with teams
+    public function teams()
+    {
+        return $this->hasMany(Team::class);
+    }
+    // relation company with roles
+    public function roles()
+    {
+        return $this->hasMany(Role::class);
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */    protected $fillable = [
+        'name',
+        'company_id',
+    ];
+    // relation role with company
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
